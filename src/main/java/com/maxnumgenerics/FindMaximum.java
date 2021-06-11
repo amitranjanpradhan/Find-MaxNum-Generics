c016688b53b1ae0c24a38328dffba71264df13c0
amitranjan <amitranjanpradhan456@gmail.com>
2021-06-11 21:37:15 +0530

    Added: UC3 code to find max string and three testcases

--- src/main/java/com/maxnumgenerics/FindMaximum.java
+++ src/main/java/com/maxnumgenerics/FindMaximum.java
@@ -5,6 +5,7 @@ public class FindMaximum {
      * here i have created a parameterised constructor having given parameters.
      * First i have taken numberOne is maxNumber
      * then i used compareTo method which will compare with numberTwo..and gradually compare with numberThree also.
+     *
      * @param numberOne
      * @param numberTwo
      * @param numberThree
@@ -20,8 +21,10 @@ public class FindMaximum {
         }
         return maxNumber;
     }
+
     /**
      * As above it'll work the same but having only float values.
+     *
      * @param numberOne
      * @param numberTwo
      * @param numberThree
@@ -37,5 +40,23 @@ public class FindMaximum {
             maxNumber = numberThree;
         }
         return maxNumber;
+    }
+    /**
+     * As above it'll work the same but having only String values.
+     * @param numberOne
+     * @param numberTwo
+     * @param numberThree
+     * @return
+     */
+
+    public String findMaxString(String numberOne, String numberTwo, String numberThree) {
+        String maxString = numberOne;
+        if (numberTwo.compareTo(maxString) > 0) {
+            maxString = numberTwo;
+        }
+        if (numberThree.compareTo(maxString) > 0) {
+            maxString = numberThree;
+        }
+        return maxString;
     }
 }
